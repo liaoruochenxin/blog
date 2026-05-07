8141bd36cb908bc6c0a6078679612ac0c0066b46
蒋炜 <2196716491@qq.com>
2026-05-07 19:02:54 +0800

    feat(user): 增加用户登录VO字段，并添加注解格式化对应字段，使用Jackson3标准的注解

--- src/main/java/com/xj/blogs/model/vo/UserLoginVO.java
+++ src/main/java/com/xj/blogs/model/vo/UserLoginVO.java
@@ -1,18 +1,43 @@
 package com.xj.blogs.model.vo;
 
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import tools.jackson.databind.annotation.JsonSerialize;
+import tools.jackson.databind.ser.std.ToStringSerializer;
 
 @Data
 @Schema(description = "登录用户VO")
 public class UserLoginVO {
 
+    @Schema(description = "用户ID")
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long id;
+
     @Schema(description = "用户账号")
     private String userAccount;
+
+    @Schema(description = "用户昵称")
+    private String userName;
 
     @Schema(description = "用户头像")
     private String userAvatar;
 
     @Schema(description = "用户简介")
     private String userProfile;
+
+    @Schema(description = "用户角色")
+    private String userRole;
+
+    @Schema(description = "创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+    @Schema(description = "更新时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateTime;
 }
